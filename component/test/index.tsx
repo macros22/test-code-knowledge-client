69ac4fa4eee054b14036696f7437a66787f5ee2b
max99xam <pepetrov123@list.ru>
2022-01-19 18:42:52 +0300

    added base test js functionality

--- component/test/index.tsx
+++ component/test/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './test.module.css';
+import styles from './test.module.scss';
 import Code from '../code';
 
 
@@ -9,7 +9,12 @@ export const answers = [
     "[object]", "[null]", "[undefined]", "Error"
 ];
 
-function Answers() {
+
+interface IProps {
+    answers: string[];
+}
+
+const Answers: React.FC<IProps> = ({ answers }) => {
     const [checkedState, setCheckedState] = React.useState<boolean[]>(
         new Array(answers.length).fill(false)
     );
@@ -53,22 +58,80 @@ function Answers() {
 
 const exampleCode = `
 console.log(typeof null)
-
-// Historical error!
-typeof null === "object" // true
 `.trim();
+
+
+
+
+const questions = [
+    {
+        question: "Что будет выведено в консоль?",
+        exampleCode: `console.log(typeof null)`.trim(),
+        answers: ["[object]", "[null]", "[undefined]", "Error"],
+        answer: "[object]"
+    },
+    {
+        question: "Что будет выведено в консоль?",
+        exampleCode: `function func() {
+                            return 0;
+                      }
+
+                      console.log(typeof func)`,
+        answers: ["[object]", "[function]", "[undefined]", "Error"],
+        answer: "[function]"
+    },
+    {
+        question: "Что будет выведено в консоль?",
+        exampleCode: `console.log(typeof 1/0)
+                        `.trim(),
+        answers: ["Infinity", "NaN", "[number]", "Error"],
+        answer: "NaN"
+    }
+]
 
 const Test: React.FC = () => {
 
+    const [currentQuestion, setCurrentQuestion] = React.useState<number>(0);
+
+    const backButtonHandler = () => {
+        if (currentQuestion > 0) {
+            setCurrentQuestion(prev => prev - 1)
+        }
+    }
+
+    const nextButtonHandler = () => {
+        if (currentQuestion < questions.length - 1) {
+            setCurrentQuestion(prev => prev + 1)
+        }
+    }
+
     return (
-        <div className={styles.article}>
+        <div className={styles.wrapper}>
 
+            <div className={styles.questionNumbers}>
+                {questions.map((_, index) => {
 
+                    let spanStyle = '';
+                    if (currentQuestion == index) {
+                        spanStyle = styles.currentItem;
+                    } else if (currentQuestion < index) {
+                        spanStyle = styles.nextItem;
+                    }
+
+                    return (
+                        <span className={spanStyle} key={index ** 2}>{index}</span>
+                    );
+                })}
+            </div>
             <div className={styles.content}>
-                <h3>{question}</h3>
-                <Code exampleCode={exampleCode} />
+                <h3 className={styles.questionTitle}>{questions[currentQuestion].question}</h3>
+                <Code exampleCode={questions[currentQuestion].exampleCode} />
 
-                <Answers />
+                <Answers answers={questions[currentQuestion].answers} />
+            </div>
+            <div className={styles.buttons}>
+                <button onClick={backButtonHandler}>Назад</button>
+                <button onClick={nextButtonHandler}>Дальше</button>
             </div>
         </div>
     )
