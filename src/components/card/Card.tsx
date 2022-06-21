0ffa407d7e8f7482a125fd6c885eab2867b13dd2
max99xam <pepetrov123@list.ru>
2022-06-21 13:11:45 +0300

    updated test page styles"

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,17 +1,17 @@
-import { CardProps } from './Card.props';
-import styles from './Card.module.scss';
-import cn from 'clsx';
-import { ForwardedRef, forwardRef } from 'react';
+import { CardProps } from "./Card.props";
+import styles from "./Card.module.scss";
+import cn from "clsx";
+import { ForwardedRef, forwardRef } from "react";
 
-export const Card = forwardRef(({ color = 'white', children, className, ...props }: CardProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
-	return (
-		<div className={cn(styles.card, className, {
-			// [styles.blue]: color == 'blue'
-		})}
-			ref={ref}
-			{...props}
-		>
-			{children}
-		</div>
-	);
-});
+export const Card = forwardRef(
+  (
+    { children, className, ...props }: CardProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ): JSX.Element => {
+    return (
+      <div className={cn(styles.card, className, {})} ref={ref} {...props}>
+        {children}
+      </div>
+    );
+  }
+);
