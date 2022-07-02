561f6e4fc42fe30c20746d9bba1513b79c1c2eb9
max99xam <pepetrov123@list.ru>
2022-07-03 00:44:31 +0300

    added adaptivefor mobile to header

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -2,27 +2,52 @@ import React from "react";
 import styles from "./Header.module.scss";
 import Link from "next/link";
 import { HeaderProps } from "./Header.props";
+import cn from "clsx";
 
 export const Header = (): HeaderProps => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen((prev) => !prev);
+  };
+
   return (
-      <header className={styles.header}>
-        <div className={styles.container}>
-          <div className={styles.headerBody}>
-            <Link href="/">
-              <a className={styles.logo}>Test JavaScript knowledge</a>
-            </Link>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.headerBody}>
+          <Link href="/">
+            <a className={styles.logo}>Test JavaScript knowledge</a>
+          </Link>
 
-            <div className={styles.links}>
-              <Link href="/test">
-                <a className={styles.link}>Test</a>
-              </Link>
-
-              <Link href="/questions">
-                <a className={styles.link}>All Questions</a>
-              </Link>
-            </div>
+          <div
+            onClick={toggleMobileMenu}
+            className={cn(styles.headerBurger, {
+              [styles.active]: isMobileMenuOpen,
+            })}
+          >
+            <span></span>
           </div>
+
+          <nav
+            className={cn(styles.headerMenu, {
+              [styles.headerMenuActive]: isMobileMenuOpen,
+            })}
+          >
+            <ul>
+              <li>
+                <Link href="/questions">
+                  <a className={styles.link}>All Questions</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/test">
+                  <a className={styles.link}>Test</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </header>
+      </div>
+    </header>
   );
 };
