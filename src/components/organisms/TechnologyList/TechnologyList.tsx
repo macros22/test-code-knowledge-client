91936d98bab29772e54f4917e8b21d46f96d22ee
max99xam <pepetrov123@list.ru>
2022-07-07 15:12:36 +0300

    updated TechnologyList styles

--- src/components/organisms/TechnologyList/TechnologyList.tsx
+++ src/components/organisms/TechnologyList/TechnologyList.tsx
@@ -4,6 +4,8 @@ import TypeScriptLogo from 'assets/pictures/ts-logo.png';
 import NodeJsLogo from 'assets/pictures/node-js-logo.png';
 import styles from './TechnologyList.module.scss';
 import { TechnologyListProps } from './TechnologyList.props';
+import { Button } from 'components/atoms/Button/Button';
+import Tag from 'components/atoms/Tag/Tag';
 
 const technologyList = [
 	{
@@ -15,7 +17,7 @@ const technologyList = [
 		src: TypeScriptLogo,
 	},
 	{
-		name: 'Nodejs',
+		name: 'NodeJs',
 		src: NodeJsLogo,
 	},
 ];
@@ -23,22 +25,27 @@ const technologyList = [
 export const TechnologyList = ({
 	questionsListsSizes,
 }: TechnologyListProps): JSX.Element => {
-	console.log(questionsListsSizes);
 	return (
 		<div className={styles.wrapper}>
 			{technologyList.map((technology) => {
 				return (
 					<div className={styles.card}>
-						<h4>
-							Questions:{' '}
-							{questionsListsSizes[technology.name.toLocaleLowerCase()]}
-						</h4>
+						<h3 className={styles.cardTitle}>{technology.name}</h3>
 						<Image
 							className={styles.img}
 							layout="responsive"
 							src={technology.src}
 							alt={technology.name}
 						/>
+						<div className={styles.cardContainer}>
+							<Button appearance="ghost">Show questions</Button>
+							<Tag size="lg" className={styles.cardTag} color="primary">
+								{questionsListsSizes[technology.name.toLocaleLowerCase()]}
+							</Tag>
+							<Button className={styles.cardButton} appearance="primary">
+								Pass the test
+							</Button>
+						</div>
 					</div>
 				);
 			})}
