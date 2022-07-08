646f3f97cf2425b9945d58dc5942a827be4159b2
max99xam <pepetrov123@list.ru>
2022-07-08 21:52:30 +0300

    added valid Github link

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
 		<footer className={cn(className, styles.footer)} {...props}>
 			<div>Code knowledge test © 2021 - {format(new Date(), 'yyyy')}</div>
-			<a href="#" target="_blank">
+			<a href="https://github.com/kramax42/test-code-knowledge-client" target="_blank">
 				Github
 			</a>
 		</footer>
