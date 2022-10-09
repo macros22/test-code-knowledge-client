e994b6547a8841ee978852f5e788a4c3658f7e44
kramax42 <kramax42@yandex.by>
2022-10-09 19:53:16 +0300

    added progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import type { AppProps } from 'next/app';
+import NextNProgress from "nextjs-progressbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'assets/globals.scss';
 
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Component {...pageProps} />
+		<>
+			<Component {...pageProps} />
+			<NextNProgress
+				color="#09a180"
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={2}
+				showOnShallow={true}
+			/>
+		</>
+
 	);
 }
 
