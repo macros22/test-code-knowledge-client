c153533bbf0123e5e522269001fb5b45046a641d
kramax42 <kramax42@yandex.by>
2022-10-22 00:41:47 +0300

    refactored Auth component

--- src/components/auth/Auth/Auth.tsx
+++ src/components/auth/Auth/Auth.tsx
@@ -5,19 +5,22 @@ import dynamic from "next/dynamic";
 const SignIn = dynamic(() => import('../SignIn/SignIn'));
 const SignUp = dynamic(() => import('../SignUp/SignUp'));
 
-
 export const Auth = ({ authMode }: IAuthProps): JSX.Element => {
     return (
-        <>
-            <Container >
-                <div className='mb-3' style={{ textAlign: 'center' }}>
-                    {authMode == 'sign-in' ? <h2>Sign In</h2> : <h2>Sign Up</h2>}
-                </div>
-                <Card >
-                    <Card.Body>
-                        {authMode == 'sign-in' ? <SignIn /> : <SignUp />}
-                    </Card.Body>
-                </Card>
-            </Container>
-        </>);
+        <Container >
+            <div className='mb-3' style={{ textAlign: 'center' }}>
+                {authMode == 'sign-in'
+                    ? <h2>Sign In</h2>
+                    : <h2>Sign Up</h2>}
+            </div>
+            <Card >
+                <Card.Body>
+                    {authMode == 'sign-in'
+                        ? <SignIn />
+                        : <SignUp />
+                    }
+                </Card.Body>
+            </Card>
+        </Container>
+    );
 };
