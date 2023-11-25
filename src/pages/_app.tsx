7be329e6dfb20574428d87268e20b7db9f172e2d
Max <kramax42@yandex.by>
2023-11-25 17:35:52 +0300

    feat: upgrade node engine to 20

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,11 @@ import NextNProgress from 'nextjs-progressbar';
 import '@/app2/global.css'
 import { ThemeContextProvider } from '@/contexts/theme.context'
 import { ThemeProvider } from 'next-themes'
+import { isDev, isProd } from '@/lib/config';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  console.log('isDev', isDev)
+  console.log('isProd', isProd)
   return (
     <ThemeContextProvider>
       <ThemeProvider
